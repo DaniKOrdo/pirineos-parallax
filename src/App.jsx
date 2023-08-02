9cc4cb8d1ca33f73fec98983f60d23d45ded1b9a
DaniK <danikordonezgarcia@gmail.com>
2023-08-02 22:51:31 +0200

    optimize images to webp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Parallax from './components/Parallax'
 
 function App() {
--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -2,43 +2,23 @@ import Atropos from "atropos/react";
 
 export default function App() {
   return (
-    <div className="">
+    <div className="pt-12">
       <Atropos
         className="atropos atropos-header w-full atropos-rotate-touch-scroll-y"
         activeOffset={40}
-        shadowScale={1.10}
+        shadowScale={1.1}
         highlight={true}
       >
         <img
           className="atropos-header-spacer"
-          src="src/images/img-a-6.png"
+          src="src/images/img-a-6.webp"
           data-atropos-offset="-4.5"
         />
-        <img
-          className="layer"
-          src="src/images/img-a-5.png"
-          data-atropos-offset="-3.5"
-        />
-        <img
-          className="layer"
-          src="src/images/img-a-4.png"
-          data-atropos-offset="-2"
-        />
-        <img
-          className="layer"
-          src="src/images/img-a-3.png"
-          data-atropos-offset="-1"
-        />
-        <img
-          className="layer"
-          src="src/images/img-a-2.png"
-          data-atropos-offset="0"
-        />
-        <img
-          className="layer"
-          src="src/images/img-a-1.png"
-          data-atropos-offset="1"
-        />
+        <img src="src/images/img-a-5.webp" data-atropos-offset="-3.5" />
+        <img src="src/images/img-a-4.webp" data-atropos-offset="-2.5" />
+        <img src="src/images/img-a-3.webp" data-atropos-offset="-2" />
+        <img src="src/images/img-a-2.webp" data-atropos-offset="-1" />
+        <img src="src/images/img-a-1.webp" data-atropos-offset="1" />
       </Atropos>
     </div>
   );
