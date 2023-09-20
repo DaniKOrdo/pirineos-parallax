fa2798071b7cee0c0a6232b8fcd3b435da9641bb
DaniKOrdo <hola@danik.dev>
2023-09-20 16:14:19 +0200

    add all the images

---
+++ src/utils/images.js
@@ -0,0 +1,42 @@
+const images = {
+  fullImages: [
+    "src/images/img-a-0.webp",
+    "src/images/img-a-0.webp",
+    "src/images/img-a-0.webp",
+  ],
+  0: {
+    fullImage: "src/images/img-a-0.webp",
+    parallaxImages: [
+      { url: "src/images/img-a-6.webp", offset: "-4.5" },
+      { url: "src/images/img-a-5.webp", offset: "-3.5" },
+      { url: "src/images/img-a-4.webp", offset: "-2.5" },
+      { url: "src/images/img-a-3.webp", offset: "-2" },
+      { url: "src/images/img-a-2.webp", offset: "-1" },
+      { url: "src/images/img-a-1.webp", offset: "1" },
+    ],
+  },
+  1: {
+    fullImage: "src/images/img-a-0.webp",
+    parallaxImages: [
+      { url: "src/images/img-a-6.webp", offset: "-4.5" },
+      { url: "src/images/img-a-5.webp", offset: "-3.5" },
+      { url: "src/images/img-a-4.webp", offset: "-2.5" },
+      { url: "src/images/img-a-3.webp", offset: "-2" },
+      { url: "src/images/img-a-2.webp", offset: "-1" },
+      { url: "src/images/img-a-1.webp", offset: "1" },
+    ],
+  },
+  2: {
+    fullImage: "src/images/img-a-0.webp",
+    parallaxImages: [
+      { url: "src/images/img-a-6.webp", offset: "-4.5" },
+      { url: "src/images/img-a-5.webp", offset: "-3.5" },
+      { url: "src/images/img-a-4.webp", offset: "-2.5" },
+      { url: "src/images/img-a-3.webp", offset: "-2" },
+      { url: "src/images/img-a-2.webp", offset: "-1" },
+      { url: "src/images/img-a-1.webp", offset: "1" },
+    ],
+  },
+}
+
+export default images
