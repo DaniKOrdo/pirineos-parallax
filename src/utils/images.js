a315fe2648d05b1bfe620bed3f7cd536026acb13
DaniK <danikordonezgarcia@gmail.com>
2023-09-21 17:39:42 +0200

    changed structure and names

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,40 +1,52 @@
 const images = {
   fullImages: [
-    "src/images/img-a-0.webp",
-    "src/images/img-a-0.webp",
-    "src/images/img-a-0.webp",
+    "src/images/piri-1.webp",
+    "src/images/piri-2.webp",
+    "src/images/piri-3.webp",
+    "src/images/piri-4.webp",
+    "src/images/piri-5.webp",
   ],
   0: {
-    fullImage: "src/images/img-a-0.webp",
     parallaxImages: [
-      { url: "src/images/img-a-6.webp", offset: "-4.5" },
-      { url: "src/images/img-a-5.webp", offset: "-3.5" },
-      { url: "src/images/img-a-4.webp", offset: "-2.5" },
-      { url: "src/images/img-a-3.webp", offset: "-2" },
-      { url: "src/images/img-a-2.webp", offset: "-1" },
-      { url: "src/images/img-a-1.webp", offset: "1" },
+      { url: "src/images/a-6.webp", offset: "-4.5" },
+      { url: "src/images/a-5.webp", offset: "-3.5" },
+      { url: "src/images/a-4.webp", offset: "-2.5" },
+      { url: "src/images/a-3.webp", offset: "-2" },
+      { url: "src/images/a-2.webp", offset: "-1" },
+      { url: "src/images/a-1.webp", offset: "1" },
     ],
+    infoPhoto: {
+      location: "test",
+      description: "test desct"
+    }
   },
   1: {
-    fullImage: "src/images/img-a-0.webp",
     parallaxImages: [
-      { url: "src/images/img-a-6.webp", offset: "-4.5" },
-      { url: "src/images/img-a-5.webp", offset: "-3.5" },
-      { url: "src/images/img-a-4.webp", offset: "-2.5" },
-      { url: "src/images/img-a-3.webp", offset: "-2" },
-      { url: "src/images/img-a-2.webp", offset: "-1" },
-      { url: "src/images/img-a-1.webp", offset: "1" },
+      { url: "src/images/b-6.webp", offset: "-4.5" },
+      { url: "src/images/b-5.webp", offset: "-3.5" },
+      { url: "src/images/b-4.webp", offset: "-2.5" },
+      { url: "src/images/b-3.webp", offset: "-2" },
+      { url: "src/images/b-2.webp", offset: "-1" },
+      { url: "src/images/b-1.webp", offset: "1" },
     ],
   },
   2: {
-    fullImage: "src/images/img-a-0.webp",
     parallaxImages: [
-      { url: "src/images/img-a-6.webp", offset: "-4.5" },
-      { url: "src/images/img-a-5.webp", offset: "-3.5" },
-      { url: "src/images/img-a-4.webp", offset: "-2.5" },
-      { url: "src/images/img-a-3.webp", offset: "-2" },
-      { url: "src/images/img-a-2.webp", offset: "-1" },
-      { url: "src/images/img-a-1.webp", offset: "1" },
+      { url: "src/images/c-5.webp", offset: "-3.5" },
+      { url: "src/images/c-4.webp", offset: "-2.5" },
+      { url: "src/images/c-3.webp", offset: "-2" },
+      { url: "src/images/c-2.webp", offset: "0" },
+      { url: "src/images/c-1.webp", offset: "1" },
+    ],
+  },
+  3: {
+    parallaxImages: [
+      { url: "src/images/piri-4.webp", offset: "0" }
+    ],
+  },
+  4: {
+    parallaxImages: [
+      { url: "src/images/piri-5.webp", offset: "0" }
     ],
   },
 }
