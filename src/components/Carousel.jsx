0ad1eb397df76885c008f58eed41ef76e5e123a1
DaniK <danikordonezgarcia@gmail.com>
2023-09-23 15:24:55 +0200

    change width

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import images from "../utils/images";
 
 export default function Carousel({ onCarouselPhotoClick, actualImage }) {
   return (
-    <section className="flex items-center xl:justify-center mx-auto p-3 gap-4 mt-8 md:mt-8 w-11/12 2xl:w-8/12 overflow-y-hidden overflow-x-visible">
+    <section className="flex items-center xl:justify-center mx-auto p-3 gap-4 mt-8 md:mt-8 w-11/12 2xl:w-9/12 overflow-y-hidden overflow-x-visible">
       {images.fullImages.map((image, index) => {
         const opacity = actualImage !== index ? "opacity-50 hover:opacity-90" : "";
         return (
