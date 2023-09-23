46bd4bcf14b69200976bb2630278cecb2051bfe6
DaniK <danikordonezgarcia@gmail.com>
2023-09-23 15:25:03 +0200

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 right-0 p-4 text-center text-slate-300">
+      Created with ♥️ by {" "}
+      <a
+        href="https://danik.dev/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-slate-100 transition duration-300 ease-in-out underline"
+      >
+        danik.dev
+      </a>
+    </footer>
+  );
+}
