3075bfe63b428b9d0676d79815de976fb73a836a
DaniKOrdo <hola@danik.dev>
2023-09-27 12:12:51 +0200

    add progressiveImage component

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,6 +1,7 @@
 import Atropos from "atropos/react";
+import ProgressiveImage from "react-progressive-graceful-image";
 
-export default function Parallax({ images }) {
+export default function Parallax({ images, placeHolder }) {
   return (
     <picture className="w-11/12 2xl:w-8/12 mx-auto px-1">
       <Atropos
@@ -9,12 +10,20 @@ export default function Parallax({ images }) {
         shadowScale={1.1}
       >
         {images.map((image, index) => (
-          <img
-            key={index}
+          <ProgressiveImage
             src={image.url}
-            alt={`Imagen ${index + 1}`}
-            data-atropos-offset={image.offset}
-          />
+            placeholder={placeHolder}
+            key={index}
+          >
+            {(src, loading) => (
+              <img
+                src={src}
+                alt={`Imagen ${index + 1}`}
+                data-atropos-offset={image.offset}
+                style={{ filter: loading ? 'blur(15px)' : 'blur(0)' }}
+              />
+            )}
+          </ProgressiveImage>
         ))}
       </Atropos>
     </picture>
