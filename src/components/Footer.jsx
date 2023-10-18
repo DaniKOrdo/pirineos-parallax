da6fa9ef0e3f0e39516995d79d9eb85783d2e427
DaniKOrdo <hola@danik.dev>
2023-10-18 12:12:49 +0200

    eliminate fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 p-4 text-center text-slate-300 w-64 mx-auto">
+    <footer className="bottom-0 w-64 p-4 mx-auto text-center text-slate-300">
       Created with ♥️ by {" "}
       <a
         href="https://danik.dev/"
         target="_blank"
         rel="noopener noreferrer"
-        className="hover:text-slate-100 transition duration-300 ease-in-out underline"
+        className="underline transition duration-300 ease-in-out hover:text-slate-100"
       >
         danik.dev
       </a>
