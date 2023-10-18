ab5ccb5f2904b2646b603d1f8fabcf8fe2a2717b
DaniKOrdo <hola@danik.dev>
2023-10-18 12:12:14 +0200

    add underline to hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 export default function Header() {
   return (
-    <header className="flex flex-col justify-center items-center my-8 text-transparent bg-clip-text bg-gradient-to-r from-slate-300 to-slate-500">
-      <h1 className="text-3xl md:text-7xl font-extrabold">
+    <header className="flex flex-col items-center justify-center my-8 text-transparent bg-clip-text bg-gradient-to-r from-slate-300 to-slate-500">
+      <h1 className="text-3xl font-extrabold md:text-7xl">
         PIRINEOS PARALLAX
       </h1>
-      <h2 className="text-lg md:text-xl font-semibold md:p-2">
+      <h2 className="text-lg font-semibold md:text-xl md:p-2">
         Photos by{" "}
         <a
           href="https://amercanyelles.com/"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-slate-100 transition duration-300 ease-in-out"
+          className="underline transition duration-300 ease-in-out hover:text-slate-100"
         >
           @amercanyelles
         </a>
