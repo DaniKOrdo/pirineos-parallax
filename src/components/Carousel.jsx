30f3297e7e54f6f0b6bc71fcb25f7a9461bd0da2
DaniKOrdo <hola@danik.dev>
2023-10-18 12:12:26 +0200

    add ring to actual img

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,9 +2,9 @@ import images from "../utils/images";
 
 export default function Carousel({ onCarouselPhotoClick, actualImage }) {
   return (
-    <section className="flex items-center xl:justify-center mx-auto p-3 gap-4 mt-8 md:mt-8 w-11/12 2xl:w-9/12 overflow-y-hidden overflow-x-visible">
+    <section className="flex items-center w-11/12 gap-4 p-3 mx-auto mt-8 overflow-x-visible overflow-y-hidden xl:justify-center md:mt-8 2xl:w-9/12">
       {images.fullImages.map((image, index) => {
-        const opacity = actualImage !== index ? "opacity-50 hover:opacity-90" : "";
+        const opacity = actualImage !== index ? "opacity-50 hover:opacity-90" : "ring ring-slate-400";
         return (
           <img
             key={index}
